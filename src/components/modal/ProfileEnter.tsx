06483fb24e2984222dba56cb3e72420c6789a20b
ax1lebafer <raul8917@gmail.com>
2024-10-05 16:44:03 +0400

    Настроил позиционирование модального окна профиля

--- src/components/modal/ProfileEnter.tsx
+++ src/components/modal/ProfileEnter.tsx
@@ -23,36 +23,35 @@ export default function ProfileEnter() {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div
-        className="fixed inset-0 bg-black opacity-20"
-        onClick={onClose}
-      ></div>
+    <div className="fixed inset-0 flex items-center justify-center z-10">
+      <div className="fixed inset-0 z-20" onClick={onClose}></div>
       {/* <div className="relative left-[calc(50%+266px/2)] top-[-120px] opacity-100"> */}
-      <div className="relative left-[447px] top-[-50px] opacity-100">
-        <form
-          className="w-[266px] p-[30px] bg-[white] rounded-[30px] flex flex-col items-center"
-          action="#"
-        >
-          <div className="w-[206px] h-[50px]  mb-[34px]">
-            <div className="flex flex-col gap-[10px] text-center text-[18px] font-normal leading-4">
-              <p className="text-[#000000]">{user.name}</p>
-              <p className="text-[#999999]">{user.email}</p>
+      <div className="relative min-w-[1160px] h-full">
+        <div className="absolute top-[100px] right-0 z-30">
+          <form
+            className="w-[266px] p-[30px] bg-[white] rounded-[30px] flex flex-col items-center"
+            action="#"
+          >
+            <div className="w-[206px] h-[50px]  mb-[34px]">
+              <div className="flex flex-col gap-[10px] text-center text-[18px] font-normal leading-4">
+                <p className="text-[#000000]">{user.name}</p>
+                <p className="text-[#999999]">{user.email}</p>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col gap-[10px]">
-            <ButtonLink
-              text="Мой профиль"
-              className="mt-[0px] w-[206px]"
-              to="/profile"
-            />
-            <ButtonLink
-              text="Выйти"
-              className="mt-[0px] w-[206px] bg-transparent border border-black hover:bg-[#F7F7F7] hover:text-black active:bg-[#E9ECED] active:text-black"
-              onClick={onExit}
-            />
-          </div>
-        </form>
+            <div className="flex flex-col gap-[10px]">
+              <ButtonLink
+                text="Мой профиль"
+                className="mt-[0px] w-[206px]"
+                to={"/profile"}
+              />
+              <ButtonLink
+                text="Выйти"
+                className="mt-[0px] w-[206px] bg-transparent border border-black hover:bg-[#F7F7F7] hover:text-black active:bg-[#E9ECED] active:text-black"
+                onClick={onExit}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );

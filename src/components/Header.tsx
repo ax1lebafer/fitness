feb3ff4dbe6f90eaaf257eef58818e3fd4bb5645
OlegGorin <olgorin@rambler.ru>
2024-10-05 18:39:21 +0200

    Позиционирование модалки входа в профиль

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,21 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import ButtonLink from "./ui/ButtonLink.tsx";
 import { appRoutes } from "../lib/appRoutes.ts";
 import { useUser } from "../hooks/useUser.ts";
+import { useState } from "react";
+import ProfileEnter from "./modal/ProfileEnter.tsx";
 
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
   const { user, isEntering, isProfile } = useUser();
 
-  console.log("Header. isProfile", isProfile);
+  const [openModal, setOpenModal] = useState(false);
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
+
+  // console.log("Header. isProfile", isProfile);
 
   let UserName = "";
 
@@ -20,18 +28,12 @@ export default function Header() {
     navigate(appRoutes.SIGNIN, { state: { backgroundLocation: location } });
   };
 
-  const openProfileEnterModal = () => {
-    navigate(appRoutes.PROFILE_ENTER, {
-      state: { backgroundLocation: location },
-    });
-  };
-
   return (
     <>
-      {/* <meta name="viewport" content="width=375, initial-scale=1" /> */}
+      {/* <header className="flex justify-between pt-[30px] pb-[60px] items-center"> */}
       <header>
         <div className="w-[343px] xl:w-auto mx-[calc((100%-343px)/2)] xl:mx-0 items-center content-center justify-items-center">
-          <div className="flex justify-between pt-[30px] items-center content-center justify-items-center">
+          <div className="flex justify-between pt-[30px] items-center">
             <div>
               <Link to="/">
                 <img
@@ -49,26 +51,17 @@ export default function Header() {
               )}
             </div>
             {!isEntering ? (
-              <ButtonLink
-                text={"Войти"}
-                onClick={openSignInModal}
-                className="text-[18px]"
-              />
+              <ButtonLink text={"Войти"} onClick={openSignInModal} />
             ) : (
               <div className="w-auto h-[35px] xl:h-[50px] flex flex-row gap-[12px]">
                 <div className="w-[35px] xl:w-[50px]">
-                  <img
-                    src="/img/avatar-small.svg"
-                    alt="avatar"
-                    // width={35}
-                    // height={35}
-                  />
+                  <img src="/img/avatar-small.svg" alt="avatar" />
                 </div>
                 <div className="hidden xl:block xl:text-center xl:text-2xl xl:pt-[6px]">
                   {UserName}
                 </div>
                 <button
-                  onClick={openProfileEnterModal}
+                  onClick={() => setOpenModal(true)}
                   className="w-[15px] h-[35px] xl:h-[50px] bg-[#e5e5e5] cursor-point inline-block"
                   type="button"
                 >
@@ -79,8 +72,10 @@ export default function Header() {
                     height={15}
                   />
                 </button>
+                {/* </div> */}
               </div>
             )}
+            {openModal && <ProfileEnter closeModal={closeModal} />}
           </div>
           <div className="h-[34px] xl:h-[60px]"></div>
         </div>

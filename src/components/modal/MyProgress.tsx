195b901df5eb5464983b66b5670c31b55ebab011
Acer <regina-sabitova-1998@mail.ru>
2024-10-08 13:15:33 +0500

    добавила стилизацию

--- src/components/modal/MyProgress.tsx
+++ src/components/modal/MyProgress.tsx
@@ -1,7 +1,7 @@
 import ButtonLink from "../ui/ButtonLink";
 import { arrayTrainingProps } from "../../pages/MainTraining";
 import { useState } from "react";
-import { InputProgressForm } from "../inputProgressForm";
+import { InputProgressForm } from "../InputProgressForm";
 
 type TypeMyProgressProps = {
   arrayTraining: arrayTrainingProps[];
@@ -12,33 +12,34 @@ export default function MyProgress({
   arrayTraining,
   handleSaveChanges,
 }: TypeMyProgressProps) {
-  
-
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-10">
+    <div className="fixed inset-0 flex items-center justify-center w-full h-full bg-black bg-opacity-20 z-20">
       <div
-        className="fixed inset-0 bg-black opacity-20"
-        /* onClick={onClose} */
-      ></div>
-      <div className="overflow-x-hidden bg-white z-20">
-        <h3 className="font-skyeng text-[32px] text-black mb-12">
-          Мой прогресс
-        </h3>
-        <div className="w-[237px] lg:w-[320px] max-h-[350px]  mb-[34px]">
-          {arrayTraining.map((target, i) => {
-            return (
-              <InputProgressForm
-                key={i}
-                exerciseName={target.target}
-                value={value}
-                onChange={(e) => {setValue(e.target.value)}}
-              />
-            );
-          })}
-          <ButtonLink text={"Сохранить"} onClick={handleSaveChanges} />
-        </div> 
+        className="fixed top-[calc(50%-(487px/2))] left-[calc(50%-(343px/2))] lg:top-[calc(50%-(596px/2))] lg:left-[calc(50%-(426px/2))]
+       bg-white  rounded-[30px] shadow-def w-[343px] p-[30px] lg:w-[426px] lg:p-10"
+      >
+        <div className="overflow-x-hidden ">
+          <h3 className="font-skyeng text-[32px] text-black mb-12">
+            Мой прогресс
+          </h3>
+          <div className="w-[237px] lg:w-[320px] max-h-[350px]  mb-[34px]">
+            {arrayTraining.map((target, i) => {
+              return (
+                <InputProgressForm
+                  key={i}
+                  exerciseName={target.target}
+                  value={value}
+                  onChange={(e) => {
+                    setValue(e.target.value);
+                  }}
+                />
+              );
+            })}
+            <ButtonLink text={"Сохранить"} onClick={handleSaveChanges} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/modal/MyProgressCounted.tsx
+++ src/components/modal/MyProgressCounted.tsx
@@ -1,13 +1,19 @@
 export default function MyProgressCounted() {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div
-            className="fixed inset-0 bg-black opacity-20"
-            
-          ></div>
-          <div className="absolute left-[calc(50%-(360px/2))] top-[calc(50%-(527px/2))] opacity-100">
-            <p>Ваш прогресс засчитан</p>
-          </div>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center w-full h-full bg-black bg-opacity-20 z-20">
+      <div className="fixed top-[calc(50%-(252px/2))] left-[calc(50%-(343px/2))] lg:top-[calc(50%-(278px/2))] lg:left-[calc(50%-(426px/2))]">
+        <div className="flex flex-col items-center bg-white w-[343px] p-[30px] lg:w-[426px] lg:p-10 rounded-[30px] shadow-def text-center">
+          <h3 className="font-skyeng text-[40px] text-black mb-10">
+            Ваш прогресс засчитан!
+          </h3>
+          <img
+            src="/img/icons/success.svg"
+            alt="Прогресс засчитан"
+            width={58}
+            height={58}
+          ></img>
         </div>
-      );
+      </div>
+    </div>
+  );
 }

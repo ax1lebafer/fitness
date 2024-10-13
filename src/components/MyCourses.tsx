b674eed75263ae24a39b6b7aa4cf4c0491912209
ax1lebafer <raul8917@gmail.com>
2024-10-14 00:02:14 +0400

    Add adaptive classes to MyCourses component

--- src/components/MyCourses.tsx
+++ src/components/MyCourses.tsx
@@ -3,11 +3,11 @@ import Card from "./Card.tsx";
 export default function MyCourses() {
   return (
     <section className="mb-10">
-      <h2 className="text-left text-[40px] font-semibold mt-[60px] mb-10">
+      <h2 className="text-left text-[24px] font-semibold mt-[60px] mb-6 xl:text-[48px] xl:mb-10">
         Мои курсы
       </h2>
 
-      <ul className="flex gap-10">
+      <ul className="flex gap-6 flex-col xl:flex-row xl:gap-10">
         <Card name={"Стэп-аэробика"} id={"6i67sm"} />
         <Card name={"Стэп-аэробика"} id={"6i67sm"} />
         <Card name={"Стэп-аэробика"} id={"6i67sm"} />
